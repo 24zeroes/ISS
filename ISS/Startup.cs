7fc94f9a7c03433b7a8b1a66b1e955724ca819a7
Viktor <v.bateykin@rts-tender.ru>
2017-04-14 16:55:53 +0300

    Regions
    Regions added to ISS Main method

--- ISS/Startup.cs
+++ ISS/Startup.cs
@@ -16,28 +16,44 @@ namespace ISS
     {
         static void Main(string[] args)
         {
+            #region CRYPTO
+
             var SecCore = new SecurityCore(@"InputFiles/", @"OutputFiles/", false);
+
             //Initial config
             var ISSKeyPair = PublicKeyBox.GenerateKeyPair();
+
             KeyPair SecurityResponse = SecCore.RegisterService("ISS", new List<string> { "ISS" }, ISSKeyPair.PublicKey);
+
             byte[] ISSNonce = SecurityResponse.PublicKey;
+
             byte[] SecPublicKey = SecurityResponse.PrivateKey;
+
             byte[] EncryptedRequest =
                 PublicKeyBox.Create("ISS", ISSNonce, ISSKeyPair.PrivateKey, SecPublicKey);
+
             JToken ISSConfig = SecCore.GetProtectedInfo("ISS", EncryptedRequest);
 
+
+            #endregion
+
+            #region App1_SCHEDULER
             //Testing to schedule app1
             Task perdiodicTask = PeriodicTaskFactory.Start(() =>
-                {
-                    using (var testingApp1 = new App1()) testingApp1.Run();
+            {
+                using (var testingApp1 = new App1()) testingApp1.Run();
 
-                }, intervalInMilliseconds: 2000, // fire every two seconds...
+            }, intervalInMilliseconds: 2000, // fire every two seconds...
                maxIterations: 10);           // for a total of 10 iterations...
             perdiodicTask.ContinueWith(_ =>
             {
                 using (var testingApp1 = new App1()) testingApp1.Run();
 
             }).Wait();
+
+    
+            #endregion
+
         }
 
 
