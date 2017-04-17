8c2c97d3f1aebed53917177387a480d08eb3525e
Viktor <v.bateykin@rts-tender.ru>
2017-04-17 15:46:42 +0300

    Added datalayer Group class.
    And ref from App1 to DataLayer

--- App1/App1.cs
+++ App1/App1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AppPattern;
 using Newtonsoft.Json.Linq;
 using SecurityProvider;
 using Sodium;
+using DataLayer;
+using DataLayer.CubeMonitoring;
 
 namespace Test
 {
@@ -35,7 +38,7 @@ namespace Test
 
         public override void InitialiseInputData()
         {
-
+            var dp = new CubeMonitoring();
         }
 
         public override void ProcessData()
