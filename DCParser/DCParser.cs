689f587922e93db3490da965d83485241a0b087e
Viktor <v.bateykin@rts-tender.ru>
2017-04-18 13:55:59 +0300

    DC Parser project initial

---
+++ DCParser/DCParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppPattern;
+using LoggingProvider;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SecurityProvider;
+using Sodium;
+
+namespace DCParser
+{
+    public class DCParser : Application
+    {
+        [JsonIgnore]
+        private JToken DCParserConfig;
+        [JsonIgnore]
+        private JToken DCParserDbConfig;
+
+        public DCParser(ref SecurityCore SecCore, ref LoggingCore log, List<JToken> Roles)
+        {
+            this.log = log;
+            this.SecCore = SecCore;
+            this.AppKeyPair = PublicKeyBox.GenerateKeyPair();
+            try
+            {
+                KeyPair SecurityResponse = SecCore.RegisterService("DCParser", Roles, AppKeyPair.PublicKey);
+                log.Info("Registered sucessfull", this.ToString());
+            }
+            catch (Exception ex)
+            {
+                log.Exception(ex.Message, this.ToString(), this);
+            }
+        }
+        public override void GetConfiguration()
+        {
+
+        }
+
+        public override void InitialiseInputData()
+        {
+
+        }
+
+        public override void ProcessData()
+        {
+
+        }
+
+        public override void PublishResult()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
---
+++ ISS/Scheduler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoggingProvider;
+using Newtonsoft.Json.Linq;
+using SecurityProvider;
+using Test;
+
+namespace ISS
+{
+    class Scheduler
+    {
+        private JToken Interval;
+        private List<JToken> Roles;
+        private readonly SecurityCore SecCore;
+        private readonly LoggingCore log;
+
+        public Scheduler(string AppName, JToken Config, ref SecurityCore SecCore, ref LoggingCore log)
+        {
+            Interval = Config[AppName]["IntervalInMilliseconds"];
+            Roles = Config[AppName]["Roles"].ToList();
+
+            //var lol = Activator.CreateInstance("DCParser");
+            //Task perdiodicTask = PeriodicTaskFactory.Start(() =>
+            //{
+            //    using (var Application = new DCParser(ref SecCore, ref log, App1Roles))
+            //    {
+            //        try
+            //        {
+            //            testingDCParser.Run();
+            //            log.Append("App1 started", "INFO", "ISS", testingApp1);
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            log.Exception(ex.Message, "ISS", testingApp1);
+            //        }
+
+            //    }
+
+            //}, intervalInMilliseconds: DCParser_Interval.Value<int>() // fire every two seconds...
+            //       );           // for a total of 10 iterations...
+            //perdiodicTask.ContinueWith(_ =>
+            //    {
+            //        using (var testingApp1 = new App1(ref SecCore, ref log, App1Roles))
+            //        {
+            //            try
+            //            {
+            //                testingApp1.Run();
+            //                log.Append("App1 started", "INFO", "ISS", testingApp1);
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                log.Exception(ex.Message, "ISS", testingApp1);
+            //            }
+
+            //        }
+
+            //    }).Wait();
+        }
+    }
+}
