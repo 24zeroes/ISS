5105cf8dc46f4ff631d7dac27199b1111ba81d08
Viktor <v.bateykin@rts-tender.ru>
2017-04-20 09:32:54 +0300

    Methods for groups moved to separate file Groups.cs

--- DCParser/DCParser.cs
+++ DCParser/DCParser.cs
@@ -16,7 +16,7 @@ using DataLayer.CubeMonitoring;
 
 namespace Production
 {
-    public class DCParser : Application
+    public partial class DCParser : Application
     {
         [JsonIgnore] private JToken DCParserConfig;
         [JsonIgnore] private JToken DCParserDbConfig;
@@ -44,34 +44,12 @@ namespace Production
 
         public override void InitialiseInputData()
         {
-            //var Config = JsonConvert.DeserializeObject<DCConfig>(JsonConvert.SerializeObject(DCParserConfig));
 
             foreach (var Domain in DCParserConfig)
             {
-                DirectoryEntry entry;
+
+                GetDomainGroups(Domain);
         
-                
-                entry = new DirectoryEntry("LDAP://" + Domain.First["DC"].Value<string>(), Domain.First["Username"].Value<string>(), Domain.First["Password"].Value<string>());
-                
-
-                DirectorySearcher ds = new DirectorySearcher(entry);
-
-                ds.Filter = "(&(objectClass=group))";
-                try
-                {
-                    Groups.Add(new FullGroups
-                    {
-                        DomainName = Domain.Path.Replace("DCParser.",""),
-                        SearchResult = ds.FindAll(),
-                        GroupList = new List<Group>()
-                    });
-                    log.Info("Connected to domain sucessfull", Domain);
-                }
-                catch (Exception ex)
-                {
-                    log.Exception("Connection to domain failed. Exception message:" + ex.Message, this.ToString(), Domain);
-                }
-
             }
         }
 
@@ -82,57 +60,9 @@ namespace Production
 
             foreach (FullGroups Domain  in Groups)
             {
-                log.Info($"Domain {Domain.DomainName} group update started", ToString());
 
-                foreach (SearchResult group in Domain.SearchResult)
-                {
-                    using (var db = new CubeMonitoring(cubeConnectionString))
-                    {
-                        string Description = "";
-                        string Name = group.Properties["name"][0].ToString();
-                        string Path = group.Path;
+                UpdateDomainGroups(Domain);
 
-
-
-                        if (group.Properties["description"].Count != 0)
-                        {
-                            Description = group.Properties["description"][0].ToString();
-                        }
-
-                        var CurrentGroup = db.OfficeDCGroups.FirstOrDefault(g => g.GroupName == Name);
-
-                        if (CurrentGroup != null)
-                        {
-                            if (CurrentGroup.GroupDescription != Description)
-                            {
-
-                                CurrentGroup.GroupDescription = Description;
-                                CurrentGroup.GroupDateModified = DateTime.Now;
-                                db.Entry(CurrentGroup).State = EntityState.Modified;
-                                db.SaveChanges();
-                            }
-                        }
-                        else
-                        {
-
-                            var Group = new OfficeDCGroups
-                            {
-                                GroupName = Name,
-                                GroupDescription = Description,
-                                GroupDateModified = DateTime.Now,
-                                GroupPath = Path
-                            };
-                            db.Entry(Group).State = EntityState.Added;
-                            db.SaveChanges();
-
-                        }
-                    }
-                    
-
-
-                }
-
-                log.Info($"Domain {Domain.DomainName} group update complited", ToString());
             }
 
         }
---
+++ DCParser/Groups.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Application_Models.DC_Parser;
+using DataLayer.CubeMonitoring;
+using Newtonsoft.Json.Linq;
+
+namespace Production
+{
+    public partial class DCParser
+    {
+        private void GetDomainGroups(JToken Domain)
+        {
+            DirectoryEntry entry;
+
+
+            entry = new DirectoryEntry("LDAP://" + Domain.First["DC"].Value<string>(), Domain.First["Username"].Value<string>(), Domain.First["Password"].Value<string>());
+
+
+            DirectorySearcher ds = new DirectorySearcher(entry);
+
+            ds.Filter = "(&(objectClass=group))";
+            try
+            {
+                Groups.Add(new FullGroups
+                {
+                    DomainName = Domain.Path.Replace("DCParser.", ""),
+                    SearchResult = ds.FindAll(),
+                    GroupList = new List<Group>()
+                });
+                log.Info("Connected to domain sucessfull", Domain);
+            }
+            catch (Exception ex)
+            {
+                log.Exception("Connection to domain failed. Exception message:" + ex.Message, this.ToString(), Domain);
+            }
+        }
+
+        private void UpdateDomainGroups(FullGroups Domain)
+        {
+            log.Info($"Domain {Domain.DomainName} group update started", ToString());
+
+            foreach (SearchResult group in Domain.SearchResult)
+            {
+                using (var db = new CubeMonitoring(cubeConnectionString))
+                {
+                    string Description = "";
+                    string Name = group.Properties["name"][0].ToString();
+                    string Path = group.Path;
+
+
+
+                    if (group.Properties["description"].Count != 0)
+                    {
+                        Description = group.Properties["description"][0].ToString();
+                    }
+
+                    var CurrentGroup = db.OfficeDCGroups.FirstOrDefault(g => g.GroupName == Name);
+
+                    if (CurrentGroup != null)
+                    {
+                        if (CurrentGroup.GroupDescription != Description)
+                        {
+
+                            CurrentGroup.GroupDescription = Description;
+                            CurrentGroup.GroupDateModified = DateTime.Now;
+                            db.Entry(CurrentGroup).State = EntityState.Modified;
+                            db.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+
+                        var Group = new OfficeDCGroups
+                        {
+                            GroupName = Name,
+                            GroupDescription = Description,
+                            GroupDateModified = DateTime.Now,
+                            GroupPath = Path,
+
+                        };
+                        db.Entry(Group).State = EntityState.Added;
+                        db.SaveChanges();
+
+                    }
+                }
+
+
+
+            }
+
+            log.Info($"Domain {Domain.DomainName} group update complited", ToString());
+        }
+    }
+}
